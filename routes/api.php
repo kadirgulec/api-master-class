d171cc94f9bfcabb15f352cfbf61078fac143968
Kadir <kg@aks-service.de>
2025-07-07 12:54:22 +0200

    Implements basic user authentication
    Adds login and register functionality using a dedicated controller and request validation.
    
    Also adds a Ticket model, factory, migration and seeder to set up the database with some data.
    
    Creates an ApiResponses trait to handle consistent API responses.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return \App\Models\Ticket::all();
+});
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
